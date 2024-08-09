c4aceba102f0d4183dc0390d0614c8359eeb94e2
Zane Rockenbaugh <zane@liquid-labs.com>
2024-08-09 15:44:10 -0500

    change var name for better clarity

--- src/map-errors-to-http-status.mjs
+++ src/map-errors-to-http-status.mjs
@@ -2,26 +2,26 @@ const defaultMapping = {
   InvalidArgumentError : 400
 }
 
-const mapping = {}
+const customMapping = {}
 
 const mapErrorsToHTTPStatus = (errorName, status) => {
-  if (errorName === undefined) { // reset mapping to default
-    for (const prop in mapping) {
-      delete mapping[prop]
+  if (errorName === undefined) { // reset customMapping to default
+    for (const prop in customMapping) {
+      delete customMapping[prop]
     }
   } else if (errorName instanceof Error) {
-    return mapping[errorName.name] || defaultMapping[errorName.name]
+    return customMapping[errorName.name] || defaultMapping[errorName.name]
   } else if (typeof errorName === 'function') { // classes are functions
     if (status === undefined) {
-      return mapping[errorName.typeName] || defaultMapping[errorName.typeName]
-    } // else, we update the mapping
-    mapping[errorName.name] = status
+      return customMapping[errorName.typeName] || defaultMapping[errorName.typeName]
+    } // else, we update the customMapping
+    customMapping[errorName.name] = status
   } else if (typeof errorName === 'object') {
-    Object.assign(mapping, errorName) // bulk update mappings
-  } else if (status === undefined) { // return mapping value
-    return mapping[errorName] || defaultMapping[errorName]
-  } else { // both errorName and status are defined, set individual mapping
-    mapping[errorName] = status
+    Object.assign(customMapping, errorName) // bulk update customMappings
+  } else if (status === undefined) { // return customMapping value
+    return customMapping[errorName] || defaultMapping[errorName]
+  } else { // both errorName and status are defined, set individual customMapping
+    customMapping[errorName] = status
   }
 }
 
