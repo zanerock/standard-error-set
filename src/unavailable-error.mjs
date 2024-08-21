c6d2e5b53a8fa3fe663d2698dcfa894ef4d79347
Zane Rockenbaugh <zane@liquid-labs.com>
2024-08-21 10:38:29 -0500

    updated docs, tested, tweaked message, and fixed missing param for UnavailableError

--- src/unavailable-error.mjs
+++ src/unavailable-error.mjs
@@ -16,21 +16,24 @@ const UnavailableError = class extends CommonError {
    * {@link UnavailableError} constructor.
    *
    * See the [common parameters](#common-parameters) note for additional parameters.
-   * @param {object|undefined} options - The constructor options.
-   * @param {string|undefined} options.expectedTime - A short description as to when the resource might be available. E.g., 'after 1400' or 'in two hours'.
-   * @param {string|undefined} options.target - The name of the function, endpoint, service, etc. which the user is
-   *   trying to invoke. E.g., '/some/url/endpoint' or 'myFunction()'
+   * @param {object} [options = undefined] - The constructor options.
+   * @param {string} [options.expectedTime = undefined] - A short description as to when the resource might be 
+   *   available. E.g., 'after 1400' or 'in two hours'.
+   * @param {string} [options.issue = 'currently unavailable'] -
+   * @param {string} [options.target = 'target resource'] - The name of the function, endpoint, service, etc. which the 
+   *   user is trying to invoke. E.g., '/some/url/endpoint' or 'myFunction()'
    * @param {string} options.name - @hidden Used internally to set the name; falls through to {@link CommonError}
    *   constructor.`
-   * @param {object|undefined} options.options - @hidden The remainder of the options to to pass to `Error`.
+   * @param {object} [options.options = undefined] - @hidden The remainder of the options to to pass to `Error`.
    * @example
-   * new UnavailableError() // "The target is currently unavailable.
-   * // v "'/some/endpoint' is not currently available."
-   * new UnavailableError({ target: '/some/endpoint'})
-   * // v "'/some/endpoint' is not currently available, try again after 12:00 Saturday.'
+   * new UnavailableError() // "Target resource is currently unavailable.
+   * new UnavailableError({ target: '/some/endpoint'}) // "/some/endpoint is not currently available."
+   * // v "Customer DB is offline for maintenance."
+   * new UnavailableError({ target: 'customer DB', issue: 'offline for maintenance' })
+   * // v "/some/endpoint is not currently available; try again after 12:00 Saturday.'
    * new UnavailableError({ target: '/some/endpoint', expectedTime: 'after 12:00 Saturday' })
    */
-  constructor({ name = myName, ...options }) {
+  constructor({ name = myName, ...options } = {}) {
     options.message = options.message || generateMessage(options)
     super({ name, ...options })
   }
@@ -40,10 +43,10 @@ registerParent(myName, Object.getPrototypeOf(UnavailableError).name)
 
 UnavailableError.typeName = myName
 
-const generateMessage = ({ expectedTime, issue, target }) => {
-  let message = target === undefined ? 'The target ' : `'${target}' `
-  message += issue || 'is currently unavailable'
-  message += expectedTime === undefined ? '.' : `, true again ${expectedTime}.`
+const generateMessage = ({ expectedTime, issue = 'currently unavailable', target = 'target resource' }) => {
+  let message = target === undefined ? 'The target ' : `${target.charAt(0).toUpperCase() + target.slice(1)} `
+  message += `is ${issue}`
+  message += expectedTime === undefined ? '.' : `; try again ${expectedTime}.`
 
   return message
 }
