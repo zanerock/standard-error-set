f4a8b05a3e2603e3bd983dcf8a1797bd9e23df98
Zane Rockenbaugh <zane@liquid-labs.com>
2024-08-27 12:30:28 -0500

    implement and test 'rethrowIf()' function

--- src/lib/valid-error-names.mjs
+++ src/lib/valid-error-names.mjs
@@ -6,6 +6,7 @@ const nonErrorExports = [
   'mapErrorToHttpStatus',
   'mapHttpStatusToName',
   'registerParent',
+  'rethrowIf',
   'wrapError',
 ]
 
---
+++ src/test/rethrow-if.test.mjs
@@ -0,0 +1,47 @@
+import { ArgumentInvalidError } from '../argument-invalid-error'
+import { ArgumentTypeError } from '../argument-type-error'
+import { rethrowIf } from '../rethrow-if'
+
+describe('rethrowIf', () => {
+  const THROW = true
+  const NO_THROW = false
+  const noEntError = new Error()
+  const noEntCode = 'ENOENT'
+  noEntError.code = noEntCode
+  const status500Error = new Error()
+  status500Error.status = 500
+
+  test.each([
+    [undefined, undefined, NO_THROW],
+    [new Error(), undefined, NO_THROW],
+    [noEntError, { codeIs : noEntCode }, THROW],
+    [noEntError, { codeIs : [noEntCode] }, THROW],
+    [noEntError, { codeIsNot : noEntCode }, NO_THROW],
+    [noEntError, { codeIsNot : [noEntCode] }, NO_THROW],
+    [new ArgumentInvalidError(), { isNotInstanceOf : ArgumentInvalidError }, NO_THROW],
+    [new ArgumentInvalidError(), { isNotInstanceOf : [ArgumentInvalidError] }, NO_THROW],
+    [new Error(), { instanceOfNot : ArgumentInvalidError }, THROW],
+    [new Error(), { instanceOfNot : [ArgumentInvalidError] }, THROW],
+    [new ArgumentInvalidError(), { instanceOf : ArgumentInvalidError }, THROW],
+    [new ArgumentInvalidError(), { instanceOf : [ArgumentInvalidError] }, THROW],
+    [new ArgumentTypeError(), { instanceOf : ArgumentInvalidError }, THROW],
+    [new Error(), { instanceOf : ArgumentInvalidError }, NO_THROW],
+    [status500Error, { statusGt : 500 }, NO_THROW],
+    [status500Error, { statusGt : 400 }, THROW],
+    [status500Error, { statusGte : 500 }, THROW],
+    [status500Error, { statusGte : 501 }, NO_THROW],
+    [status500Error, { statusLt : 500 }, NO_THROW],
+    [status500Error, { statusLt : 501 }, THROW],
+    [status500Error, { statusLte : 500 }, THROW],
+    [status500Error, { statusLte : 499 }, NO_THROW],
+    [status500Error, { statusIs : 499 }, NO_THROW],
+    [status500Error, { statusIs : [499] }, NO_THROW],
+    [status500Error, { statusIs : 500 }, THROW],
+    [status500Error, { statusIsNot : 499 }, THROW],
+    [status500Error, { statusIsNot : [499] }, THROW],
+    [status500Error, { statusIsNot : 500 }, NO_THROW],
+  ])('error %p, re-throw options %p => will throw: %p', (error, options, expectThrow) => {
+    if (expectThrow === THROW) expect(() => rethrowIf(error, options)).toThrow()
+    else expect(() => rethrowIf(error, options)).not.toThrow()
+  })
+})
